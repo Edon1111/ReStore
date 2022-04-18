3e0503e97c8d485da55d596b2e347856843a743f
Edon1111 <63924185+Edon1111@users.noreply.github.com>
2022-04-18 16:40:02 +0200

    Error Handling

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,19 @@
-import Catalog from "../../features/catalog/Catalog";
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import Header from "./Header";
+import "react-toastify/dist/ReactToastify.css";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import AboutPage from "../../features/about/AboutPage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import Catalog from "../../features/catalog/Catalog";
+import HomePage from "../../features/home/HomePage";
+
+import { ToastContainer } from "react-toastify";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -22,10 +33,19 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <ToastContainer position="bottom-right" hideProgressBar />
         <CssBaseline />
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <Container>
-          <Catalog />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/catalog" component={Catalog} />
+            <Route path="/catalog/:id" component={ProductDetails} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route path="/server-error" component={ServerError} />
+            <Route component={NotFound} />
+          </Switch>
         </Container>
       </ThemeProvider>
     </>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
+export const history = createBrowserHistory();
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
